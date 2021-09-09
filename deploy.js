84659223287a514cf8274b173eb1ff7a8b1a5461
Raul A Villarreal Jr <ravillarrealjr@gmail.com>
2021-09-08 21:12:24 -0500

    changed host name

--- deploy.js
+++ deploy.js
@@ -86,7 +86,7 @@ function sshConnect() {
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
       host: '54.226.249.159',
-      username: 'ec2-user',
+      username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
     .then(function() {
